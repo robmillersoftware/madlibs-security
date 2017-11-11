51e4aa5dcc431a404dab68c420a8ce60a912a2b5
Rob Miller <robmillersoftware@gmail.com>
2017-11-11 18:35:36 -0500

    stuff

--- gulpfile.js
+++
@@ -1,5 +0,0 @@
-/**
- * The gulp build is broken out into distinct task files. This command executes index.js inside of the gulp
- * directory which runs require on every task file
- */
-require('./gulp');
