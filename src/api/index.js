6111d9f4cf3d2826dc4b2f5f65dddd1de22ce677
Ian Corritore <simaosnapper@Ians-MacBook-Pro.local>
2017-11-14 13:17:56 -0500

    added transactions for all accounts

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,12 @@ const accounts = require('./accounts.json');
 const billPay = require('./billpay.json');
 const customersRetail = require('./customersretail.json');
 const payments = require('./payments.json');
-const transactions = require('./transactions.json');
+const transactions301 = require('./transactions-301.json');
+const transactions302 = require('./transactions-302.json');
+const transactions303 = require('./transactions-303.json');
+const transactions304 = require('./transactions-304.json');
+const transactions305 = require('./transactions-305.json');
+const transactions306 = require('./transactions-306.json');
 const virtualWallet = require('./virtualwallet.json');
 
 module.exports = {
@@ -10,6 +15,13 @@ module.exports = {
 	billPay: JSON.parse(billPay),
 	customersRetail: JSON.parse(customersRetail),
 	payments: JSON.parse(payments),
-	transactions: JSON.parse(transactions),
+	transactions: {
+		301: JSON.parse(transactions301),
+		302: JSON.parse(transactions302),
+		303: JSON.parse(transactions303),
+		304: JSON.parse(transactions304),
+		305: JSON.parse(transactions305),
+		306: JSON.parse(transactions306),
+	}
 	virtualWallet: JSON.parse(virtualWallet)
 };
